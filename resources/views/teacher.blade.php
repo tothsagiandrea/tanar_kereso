df43d8f254c749ae71bf4deff7d83fc9a5f36ae4
tothsagiandrea <116187570+tothsagiandrea@users.noreply.github.com>
2023-04-13 16:46:21 +0200

    tanar sajat oldal

--- resources/views/teacher.blade.php
+++ resources/views/teacher.blade.php
@@ -2,10 +2,16 @@
 
 @section('content')
 <h1>ez van</h1>
-<div class="container-fluid teachdata">
+<div class="container-fluid teachPage">
+  <div class="container-fluid sablontanaroknak">
+    <p class="nev">Gipsz Jakab</p>
+    <p class="vegzettseg">Matematika tanár</p>
+    <img src="{{ asset('img/profile1.jpg')}}" class="rounded-circle fototanar" alt="arckep">
+    <hr color="#555457" width="20%" size="5">
+  </div>
   <p>ide dolgozz</p>
   <p>ide dolgozz</p>
   <p>ide dolgozz</p>
   <p>ide dolgozz</p>
-  <button type="button" class="btn btn-primary">Primary</button>
+  <button type="button" class="btn btn-primary">Vissza</button>
   @endsection
