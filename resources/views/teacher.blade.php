9f91114f91fdc74448f7d02adb6b817c601f9304
tothsagiandrea <116187570+tothsagiandrea@users.noreply.github.com>
2023-04-15 20:17:57 +0200

    teacherblade flexbox+css

--- resources/views/teacher.blade.php
+++ resources/views/teacher.blade.php
@@ -3,13 +3,13 @@
 @section('content')
     <h1>ez van</h1>
     <div class="container-fluid teachPage">
-      <div class="d-flex bg-light" style="height:700px">
+      <div class="d-flex bg-light sablontanaroknak" style="height:700px">
         <div class="p-2 border" style="width: 250px">
           <div class="container-fluid sablontanaroknak">
             <p class="nev">Gipsz Jakab</p>
             <p class="vegzettseg">Matematika tanár</p>
             <img src="{{ asset('img/profile1.jpg')}}" class="rounded-circle fototanar" alt="arckep">
-            <hr color="#555457" width="80%" size="5">
+            <hr color="#555457" width="60%" size="5">
             <p>Elérhetőségek</p>
             <span class="material-icons">
             phone
@@ -23,7 +23,7 @@
               location_on
             </span>
             <p>Budapest</p>
-            <hr color="#555457" width="80%" size="5">
+            <hr color="#555457" width="60%" size="5">
             <p>Óradíj</p>
             <img src="{{ asset('img/banking.png')}}" class="iconbanking" alt="icon">
             <p>5000 Ft/óra</p>
@@ -37,10 +37,26 @@
               Legyen szó tehát felzárkóztatásról, vizsga- vagy zh felkészítésről, fordulj hozzám bizalommal!</p>
           </div>
         </div>
-        <div class="p-2 border" style="width: 200px">Flex item 3</div>
+        <div class="p-2 border" style="width: 250px">Tanított tantárgyak
+          <table class="table-responsive table-secondary">
+            <tr>
+              <th  scope="col">Tantárgy</th>
+              <th style="padding: 35px" scope="col">Szint</th>
+            </tr>
+            <tr>
+              <td scope="row">Matematika</td>
+              <td style="padding-left: 35px" scope="row">Középiskola</td>
+            </tr>
+            <tr>
+              <td scope="row">Kémia</td>
+              <td style="padding-left: 35px" scope="row">Általános</td>
+            </tr>
+          </table>
+        
+        </div>
       </div>
       <div class="d-flex justify-content-center">
-        <a href="{{route('indexPage')}}"><button type="button" class="btn btn-primary">Vissza</button></a>
+        <a href="{{route('indexPage')}}"><button type="button" class="bn632-hover bn22 visszabt">Vissza</button></a>
       </div>
       
       
