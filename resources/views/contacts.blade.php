c74e47c95bca237a82d78b9473a7a1dd13553e39
tothsagiandrea <116187570+tothsagiandrea@users.noreply.github.com>
2023-04-21 14:01:43 +0200

    styling contacts page

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -21,26 +21,36 @@
           <input class="bn632-hover bn22" type="submit" value="Küldés">
         </div>
       </form>
-      
     </div>
   </div>
   
-  <div class="container map">
-    <div class="elerhetoseg">
-      <p>Elérhetőségek</p>
-        <span class="material-icons">
-        phone
-        </span><br>
-        <p>+36/35-200-4000</p>
-        <span class="material-icons">
-          email
-        </span><br>
-        <p>gipszjakab@gmail.com</p>
-        <span class="material-icons">
-          location_on
-        </span>
-        <p>1171, Budapest Borsfa utca 19.</p>
+
+
+
+
+  <div class="flexbox">
+    <div class="row">
+      <div class="elerhetoseg col">
+        <p class="pElerhetoseg">Elérhetőségek</p>
+        <hr>
+          <span class="material-icons">
+          phone
+          </span><br>
+          <p>+36/35-200-4000</p>
+          <span class="material-icons">
+            email
+          </span><br>
+          <p>gipszjakab@gmail.com</p>
+          <span class="material-icons">
+            location_on
+          </span>
+          <p>1171, Budapest Borsfa utca 19.</p>
+          <div class="col">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2696.4256549890797!2d19.271981415626207!3d47.48162067917665!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4741c6b53acafec3%3A0x701b84342b06ae32!2sBudapest%2C%20Borsfa%20u.%2019%2C%201171!5e0!3m2!1shu!2shu!4v1681732895825!5m2!1shu!2shu" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="g-maps"></iframe>
+          </div>
+      </div>
+      
     </div>
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2696.4256549890797!2d19.271981415626207!3d47.48162067917665!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4741c6b53acafec3%3A0x701b84342b06ae32!2sBudapest%2C%20Borsfa%20u.%2019%2C%201171!5e0!3m2!1shu!2shu!4v1681732895825!5m2!1shu!2shu" width="600" height="450" style="border:5px" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
   </div>
+</div>
 @endsection
--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -58,7 +58,7 @@
         <h2>TITLE HEADING</h2>
         <h5>Title description, Dec 7, 2017</h5>
         <div class="fakeimg" style="height:200px;">Image</div>
-        <p>Some text..</p>
+        <p>szöveg</p>
       </div>
       <div class="card">
         <h2>TITLE HEADING</h2>
