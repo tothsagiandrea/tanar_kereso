4f44e673fd881d3416db0800c7006746a8b54078
tothsagiandrea <116187570+tothsagiandrea@users.noreply.github.com>
2023-04-28 18:57:17 +0200

    tanár saját oldal

--- resources/views/teacher.blade.php
+++ resources/views/teacher.blade.php
@@ -1,25 +1,31 @@
 @extends('layouts.main')
 
 @section('content')
-    <h1>{{ $user->name }} adatai</h1>
+    <h1>{{ $teacher->user->name }} adatai</h1>
     <div class="container-fluid teachPage">
       <div class="d-flex bg-light sablontanaroknak" style="height:700px">
         <div class="p-2 border" style="width: 250px">
           <div class="container-fluid sablontanaroknak">
             <p class="nev">{{ $teacher->full_name }}</p>
-            <img src="{{ url("storage/profile_pics/$teacher->profile_pic_path") }}" class="rounded-circle fototanar" alt="{{ $user->name }} fényképe">
+            <img src="{{ url("storage/profile_pics/$teacher->profile_pic_path") }}" class="rounded-circle fototanar" alt="{{ $teacher->user->name }} fényképe">
             <hr color="#555457" width="60%" size="5">
             <p>Elérhetőségek</p>
             <span class="material-icons">
               email
             </span><br>
-            <p>{{ $user->email }}</p>
+            <p>{{ $teacher->user->email }}</p>
             <span class="material-icons">
               location_on
             </span>
-            @foreach ($teacher->towns as $town)
-              <div class="teacher_location_county">{{ $town->county->county }}</div>
-                <div class="teacher_location_town">{{ $town->town }}</div>
+            @foreach ($teacher->towns->unique('county') as $town_with_county)
+              <div class="teacher_location_county">{{ $town_with_county->county->county }}</div>
+              <ul>
+              @foreach ($teacher->towns as $town)
+                @if($town->county == $town_with_county->county)
+                <li class="teacher_location_county">{{ $town->town }}</li>
+                @endif
+              @endforeach
+              </ul>
             @endforeach
             <hr color="#555457" width="60%" size="5">
             <p>Óradíj</p>
@@ -38,10 +44,18 @@
               <th  scope="col">Tantárgy</th>
               <th style="padding: 35px" scope="col">Szint</th>
             </tr>
-            @foreach($teacher->grade_subjects as $grade_subject)            
+            @foreach($teacher->grade_subjects->unique('subject') as $subject) 
             <tr>
-              <td scope="row">{{ $grade_subject->subject->subject }}</td>
-              <td style="padding-left: 35px" scope="row">{{ $grade_subject->grade->grade }}</td>
+                <td scope="row">{{ $subject->subject->subject }}</td>
+                <td style="padding-left: 35px" scope="row">
+                  <ul>
+                    @foreach($teacher->grade_subjects as $grade_subject)
+                      @if($grade_subject->subject == $subject->subject)
+                      <li>{{ $grade_subject->grade->grade }}</li>
+                      @endif                    
+                    @endforeach
+                  </ul>
+                </td>
             </tr>
 						@endforeach
           </table>
